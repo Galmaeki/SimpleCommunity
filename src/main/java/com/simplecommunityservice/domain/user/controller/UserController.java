5ae5636114a6e4a5f9000e666d162664b2d4862c
Galmaeki <kjh09800@gmail.com>
2025-03-01 23:22:12 +0900

    유저 정보 수정 API 작성 - 필요 DTO 추가 - 필요 API 추가

--- src/main/java/com/simplecommunityservice/domain/user/controller/UserController.java
+++ src/main/java/com/simplecommunityservice/domain/user/controller/UserController.java
@@ -1,14 +1,14 @@
 package com.simplecommunityservice.domain.user.controller;
 
-import com.simplecommunityservice.domain.user.dto.RequestLogin;
-import com.simplecommunityservice.domain.user.dto.RequestSignup;
-import com.simplecommunityservice.domain.user.dto.ResponseDuplicate;
-import com.simplecommunityservice.domain.user.dto.ResponseUser;
+import com.simplecommunityservice.domain.user.dto.*;
 import com.simplecommunityservice.domain.user.service.UserService;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -34,5 +34,43 @@ public class UserController {
 
         response.addHeader(HttpHeaders.AUTHORIZATION, token);
         return ResponseEntity.ok(userService.getUserInfo(login));
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<ResponseUser> userInfo(@PathVariable String userId) {
+        return ResponseEntity.ok(userService.getUserInfo(userId));
+    }
+
+    @PatchMapping("/{userid}/email")
+    public ResponseEntity<Void> changeEmail(
+            @PathVariable String userid, @RequestBody RequestChangeEmail email
+    ) {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        userService.changeEmail(userid, user.getUsername(), email.email());
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @PatchMapping("/{userid}/nickname")
+    public ResponseEntity<Void> changeNickname(
+            @PathVariable String userid, @RequestBody RequestChangeNickname nickname
+    ) {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        userService.changeNickname(userid, user.getUsername(), nickname.nickname());
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @PatchMapping("/{userid}/password")
+    public ResponseEntity<Void> changeNickname(
+            @PathVariable String userid, @RequestBody RequestChangePassword password
+    ) {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        userService.changePassword(userid, user.getUsername(), password.password());
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
--- src/main/java/com/simplecommunityservice/domain/user/service/UserService.java
+++ src/main/java/com/simplecommunityservice/domain/user/service/UserService.java
@@ -10,8 +10,10 @@ import com.simplecommunityservice.security.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class UserService {
 
@@ -23,6 +25,7 @@ public class UserService {
         return new ResponseDuplicate(userRepository.existsByUserId(userId));
     }
 
+    @Transactional
     public ResponseUser signup(RequestSignup signup) {
         return toResponseUser(userRepository.save(
                 Users.builder()
@@ -49,6 +52,53 @@ public class UserService {
     }
 
     public ResponseUser getUserInfo(RequestLogin login) {
-        return toResponseUser(userRepository.findByUserId(login.userid()).orElseThrow());
+        return toResponseUser(userRepository.findByUserId(login.userid()).orElseThrow(
+                () -> new RuntimeException("유저를 찾을 수 없습니다"))
+        );
+    }
+
+    public ResponseUser getUserInfo(String userId) {
+        return toResponseUser(userRepository.findByUserId(userId).orElseThrow(
+                () -> new RuntimeException("유저를 찾을 수 없습니다")
+        ));
+    }
+
+    public void changeEmail(String pathUserId, String loginUserId, String email) {
+        Users targetUser = userRepository.findByUserId(pathUserId).orElseThrow(
+                () -> new RuntimeException("유저를 찾을 수 없습니다")
+        );
+
+        if (targetUser.getUserId().equals(loginUserId))
+            throw new RuntimeException("권한이 없습니다");
+
+        targetUser.setEmail(email);
+
+        userRepository.save(targetUser);
+    }
+
+    public void changeNickname(String pathUserId, String loginUserId, String nickname) {
+        Users targetUser = userRepository.findByUserId(pathUserId).orElseThrow(
+                () -> new RuntimeException("유저를 찾을 수 없습니다")
+        );
+
+        if (targetUser.getUserId().equals(loginUserId))
+            throw new RuntimeException("권한이 없습니다");
+
+        targetUser.setNickname(nickname);
+
+        userRepository.save(targetUser);
+    }
+
+    public void changePassword(String pathUserId, String loginUserId, String password) {
+        Users targetUser = userRepository.findByUserId(pathUserId).orElseThrow(
+                () -> new RuntimeException("유저를 찾을 수 없습니다")
+        );
+
+        if (targetUser.getUserId().equals(loginUserId))
+            throw new RuntimeException("권한이 없습니다");
+
+        targetUser.setPassword(passwordEncoder.encode(password));
+
+        userRepository.save(targetUser);
     }
 }
